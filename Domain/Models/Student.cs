c2e89c5ad5fdc0a26aa7c57f0286457a05766f41
mariama1ieva <maryamfa@code.edu.az>
2023-11-20 14:43:42 +0400

    added domain to file

---
+++ Domain/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Student:BaseEntity
+    {
+        public string Fullname { get; set; }
+        public string Adress { get; set; }
+        public int Age { get; set; }
+        public int Phone { get; set; }
+    }
+}
